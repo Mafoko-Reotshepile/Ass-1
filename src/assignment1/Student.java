dfcec691b2b7f9d38abc1a183707b3f878507c17
Mafoko-Reotshepile <ST10450048@rcconnect.edu.za>
2024-09-06 07:30:42 +0000

    Added a constructor method

--- src/assignment1/Student.java
+++ src/assignment1/Student.java
@@ -15,7 +15,16 @@ public class Student
     private int age;
     private String email;
     private String course;
-    
-    
-    
+
+// Constructor
+public Student(String name, int id, int age , String email , String course) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+        this.email = email;
+        this.course = course;
 }
+
+
+}
+
